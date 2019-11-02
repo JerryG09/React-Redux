1552f4c5d10b32c356ef8b2eae457a163806a81a
JerryG09 <phemmy09@gmail.com>
2019-11-02 12:41:21 +0100

    implemented mapStateToProps

--- my-app/src/actions/postActions.js
+++ my-app/src/actions/postActions.js
@@ -1,9 +1,11 @@
 import { FETCH_POST, NEW_POST} from './types'
 
 export const  fetchPost = () => (dispatch) => {
+  // console.log("fetching...")
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(posts => dispatch({
+      .then(posts => 
+        dispatch({
         type: FETCH_POST,
         payload: posts
       }))
--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux"
+import { fetchPost } from "../actions/postActions"
 
 class Post extends Component {
+  UNSAFE_componentWillMount() {
+    this.props.fetchPost()
+  }
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -17,4 +22,8 @@ class Post extends Component {
   }
 }
 
-export default Post
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPost})(Post)
