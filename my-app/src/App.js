20fa183f5ac3b0c202752549bd4e90b9330dc638
JerryG09 <phemmy09@gmail.com>
2019-11-02 10:49:44 +0100

    backup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Post from './components/Post'
 import PostForm from './components/PostForm'
 
+import { Provider } from "react-redux"
+import store from '../src/components/store'
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <PostForm />
-          <Post />
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <PostForm />
+            <Post />
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
